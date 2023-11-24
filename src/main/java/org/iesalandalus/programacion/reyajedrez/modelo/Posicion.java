029ea5f8d71b3f0b0f4a13ad9ff9fe001410f25f
Jose Javier Sierra <jsieber1908@gmail.com>
2023-11-24 18:06:30 +0100

    Creamos los métodos "get" y "set" para los atributos de la clase Posicion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -5,6 +5,24 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public int getFila() {
+        return fila;
+    }
+    public void setFila(int fila){
 
+        if (fila < 1 || fila > 8){
+            throw new IllegalArgumentException("Error, valor introducido erroneo. Introduce un valor entre 1 y 8");
+        }
+        this.fila = fila;
+    }
 
+    public char getColumna() { return columna; }
+
+    public void setColumna(char columna){
+
+        if (Character.toLowerCase(columna) < 'a' || Character.toLowerCase(columna) > 'h' ){
+            throw new IllegalArgumentException("Error, valor introducido erroneo. Introduce una letra entre 'a' y 'h'");
+        }
+        this.columna = columna;
+    }
 }
