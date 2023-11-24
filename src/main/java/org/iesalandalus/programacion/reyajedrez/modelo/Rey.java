b10e1900b0ccdb49bfd59819332c4513a30a6c15
Jose Javier Sierra <jsieber1908@gmail.com>
2023-11-24 19:19:18 +0100

    Creamos los métodos get y set para cada atributo de la clase Rey.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -6,5 +6,43 @@ public class Rey {
     private Posicion posicion;
     private int totalMovimientos;
 
+    public Color getColor() {
+        return color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    public void setColor(Color color) {
+
+        if (color == null){
+            throw new IllegalArgumentException("Error: El argumento color es erróneo.");
+        }
+        this.color = color;
+    }
+
+    public void setPosicion(Posicion posicion) {
+
+        if (posicion == null){
+            throw new IllegalArgumentException("Error: El argumento color es erróneo.");
+        }
+        this.posicion = posicion;
+    }
+
+    public void setTotalMovimientos(int totalMovimientos) {
+
+        if (totalMovimientos < 0){
+            throw new IllegalArgumentException("Error: El número total de movimientos no puede ser negativo.");
+        }
+        this.totalMovimientos = totalMovimientos;
+    }
+
+
+
 
 }
