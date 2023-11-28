c49f46616cc0f670b5b0664900de12481e1d6c2f
Jose Javier Sierra <jsieber1908@gmail.com>
2023-11-28 23:52:54 +0100

    Creamos el método mostrarMenu en la clase Consola.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -3,5 +3,14 @@ package org.iesalandalus.programacion.reyajedrez;
 public final class Consola {
     public Consola() {
     }
-
+    public void mostrarMenu(){
+        System.out.println("");
+        System.out.println(" | ------- Programa Rey Ajedrez v1.0 (hecho por José Javier Sierra) ------- |");
+        System.out.println("Elige una de las siguientes opciones:");
+        System.out.println("1.- Crear Rey por defecto");
+        System.out.println("2.- Crear Rey eligiendo color");
+        System.out.println("3.- Mover");
+        System.out.println("4.- Salir");
+        System.out.println("| ------------------------------------------------------------------------- |");
+    }
 }
