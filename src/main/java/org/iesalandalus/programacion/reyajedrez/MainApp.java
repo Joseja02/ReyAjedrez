c221ca7776308b50c7d570da03a6f382c5ecc7a0
Jose Javier Sierra <jsieber1908@gmail.com>
2023-11-29 19:21:03 +0100

    Creamos atributo de clase Rey.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,9 @@
 package org.iesalandalus.programacion.reyajedrez;
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
 
 public class MainApp {
-    public static void main(String[] args) {
 
+    private Rey rey;
+    public static void main(String[] args) {
     }
 }
